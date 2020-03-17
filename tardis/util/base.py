9a9fa41890dcbf409c684e9664740a710fe8ea69
Vishesh <manglavishesh64@gmailcom>
2020-03-17 15:35:51 +0530

    Numba optimize intensity_black_body

--- tardis/util/base.py
+++ tardis/util/base.py
@@ -3,6 +3,7 @@ import os
 import re
 from collections import OrderedDict
 
+import numba
 import numexpr as ne
 import numpy as np
 import pandas as pd
@@ -246,7 +247,7 @@ def create_synpp_yaml(radial1d_mdl, fname, shell_no=0, lines_db=None):
     with open(fname, 'w') as f:
         yaml.dump(yaml_reference, stream=f, explicit_start=True)
 
-
+@numba.jit(nogil=True, nopython=False, fastmath=True)
 def intensity_black_body(nu, T):
     """
     Calculate the intensity of a black-body according to the following formula
@@ -269,10 +270,8 @@ def intensity_black_body(nu, T):
     """
     beta_rad = 1 / (k_B_cgs * T)
     coefficient = 2 * h_cgs / c_cgs ** 2
-    intensity = ne.evaluate('coefficient * nu**3 / '
-                            '(exp(h_cgs * nu * beta_rad) -1 )')
+    intensity = (coefficient * nu**3) / (np.exp(h_cgs * nu * beta_rad) -1 )
     return intensity
-
 
 def species_tuple_to_string(species_tuple, roman_numerals=True):
     """
